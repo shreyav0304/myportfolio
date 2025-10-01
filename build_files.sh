d75b99f092d9c8179e6c4de51363e3675ede307b
Shreya <shreyav0304@gmail.com>
2025-10-01 14:29:59 +0530

    Fix build script and line endings

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Install dependencies
-pip install -r requirements.txt
+# Exit on error
+set -o errexit
+
+# Install dependencies using a more specific command
+python3.9 -m pip install -r requirements.txt
 
 # Run collectstatic
 python3.9 manage.py collectstatic --noinput
