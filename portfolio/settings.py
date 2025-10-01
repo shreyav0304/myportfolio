9a46869e06dbd51b9476eb597079c29e027ae41c
Shreya <shreyav0304@gmail.com>
2025-10-01 15:34:34 +0530

    REVERT: Add STATICFILES_DIRS back to settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,11 +84,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "website" / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 # Email Configuration (SendGrid)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
