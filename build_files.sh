48cdca43b5d9879c0f421a3aad967414a10c7f0c
Shreya <shreyav0304@gmail.com>
2025-10-01 14:56:03 +0530

    Downgrade Django to 4.2 and revert to Python 3.9

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,11 @@
 set -o errexit
 
 # Ensure pip is installed and upgraded
-python3.11 -m pip install --upgrade pip
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
 
-# Install dependencies
-python3.11 -m pip install -r requirements.txt
+# Install project dependencies
+python3.9 -m pip install -r requirements.txt
 
 # Run collectstatic
-python3.11 -m manage.py collectstatic --noinput
+python3.9 -m manage.py collectstatic --noinput
