feca051421ab6cbea1bfedca64e75dad7930728d
Shreya <shreyav0304@gmail.com>
2025-10-01 14:51:19 +0530

    Upgrade Python runtime to 3.11

--- build_files.sh
+++ build_files.sh
@@ -3,14 +3,11 @@
 # Exit on error
 set -o errexit
 
-# 1. Use ensurepip to install pip if it is missing
-python3.9 -m ensurepip
+# Ensure pip is installed and upgraded
+python3.11 -m pip install --upgrade pip
 
-# 2. Upgrade pip to the latest version
-python3.9 -m pip install --upgrade pip
+# Install dependencies
+python3.11 -m pip install -r requirements.txt
 
-# 3. Install project dependencies
-python3.9 -m pip install -r requirements.txt
-
-# 4. Run collectstatic
-python3.9 manage.py collectstatic --noinput
+# Run collectstatic
+python3.11 -m manage.py collectstatic --noinput
