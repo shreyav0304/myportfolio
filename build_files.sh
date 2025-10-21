4f47bcd0d44b604a75d61a7d065497e2408f1ed1
Shreya <shreyav0304@gmail.com>
2025-10-21 21:18:38 +0530

    Fix build script to use pip3 and python3 for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
